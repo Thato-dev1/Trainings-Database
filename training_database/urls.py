574d4b149a91a69e09bd8c63b3d99abecc2e6a5b
Thato Moliko <thatomoliko01@gmail.com>
2026-05-11 08:15:01 +0200

    Added a custom errorpage

--- training_database/urls.py
+++ training_database/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,4 @@ urlpatterns = [
     path('sign_out/', views.sign_out, name= 'sign_out'),
 
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
