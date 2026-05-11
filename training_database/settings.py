a02a4a3c3ec6b0bcd74bb4171a28cc78891bf822
Thato Moliko <thatomoliko01@gmail.com>
2026-05-11 08:20:40 +0200

    ignore this commit

--- training_database/settings.py
+++ training_database/settings.py
@@ -28,10 +28,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default= False)
+DEBUG = False
+# env.bool('DEBUG', default= False)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
